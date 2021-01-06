bce7e51a6148225c83ad62eb5406b8c83b518196
Joel Dsouza <joelviviandsouza20012gmail.com>
2021-01-06 23:50:24 +0530

    6/11 11:50pm

--- lib/article_card.dart
+++ lib/article_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/article_details_page.dart';
 import './provider.dart';
 import 'package:provider/provider.dart';
 
@@ -9,10 +10,16 @@ class ArticleCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final prov = Provider.of<NewsProvider>(context, listen: false);
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 3),
-      child: ListTile(
-        leading: Image.network(prov.articles[prov.curtab][index].imageUrl),
-        title: Text(prov.articles[prov.curtab][index].title),
+      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      child: GestureDetector(
+        child: ListTile(
+          leading: Image.network(prov.articles[prov.curtab][index].imageUrl),
+          title: Text(prov.articles[prov.curtab][index].title),
+        ),
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (ctx) => ArticleDetailsPage()));
+        },
       ),
     );
   }
